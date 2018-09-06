2b3eb31767595f3bd445a325781462631dc72343
Roger Terrill <rchicasterill@gmail.com>
2018-09-06 10:46:03 -0700

    Simple Mapping through an array of objects.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
-
 import './App.css';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5, 
+    objectID: 1,
+  },
+];
 
 class App extends Component {
   render() {
-    const helloWorld = {firstname:"Roger", lastname:"terrill"};
     return (
       <div className="App">
-        <h1>{helloWorld.firstname} and {helloWorld.lastname}</h1>
+        {list.map(item => {
+          return (
+            <div>{item.title}</div>
+            
+          )
+        })}
       </div>
     );
   }
