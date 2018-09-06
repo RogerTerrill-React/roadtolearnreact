447a44fed75c1c707ba957fa7e63f558511157de
Roger Terrill <rchicasterill@gmail.com>
2018-09-06 10:52:46 -0700

    Page 21, map list and access all properties.

--- src/App.js
+++ src/App.js
@@ -26,8 +26,14 @@ class App extends Component {
       <div className="App">
         {list.map(item => {
           return (
-            <div>{item.title}</div>
-            
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url} > {item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>
           )
         })}
       </div>
