80ee1b2798b6fb522c1b8cd71e2380d4ed5f7b5d
Roger Dodger <lpaddikt@gmail.com>
2018-09-10 18:30:52 -0700

    Pg 75 Added some inline CSS style

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const list_a = [
     url: 'https://redux.js.org/',
     author: 'Dan Abramov, Andrew Clark',
     num_comments: 2,
-    points: 5, 
+    points: 5,
     objectID: 1,
   },
 ];
@@ -28,12 +28,12 @@ const list_a = [
 //   }
 // }
 
-const isSearched = searchTerm => item => 
+const isSearched = searchTerm => item =>
   item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
-const Search = ({value, onChange, children}) =>
+const Search = ({ value, onChange, children }) =>
   <form>
-    {children} 
+    {children}
     <input
       type="text"
       value={value}
@@ -41,7 +41,7 @@ const Search = ({value, onChange, children}) =>
     />
   </form>
 
-const Button = ({onClick, className, children}) =>
+const Button = ({ onClick, className, children }) =>
   <button
     onClick={onClick}
     className={className}
@@ -50,24 +50,33 @@ const Button = ({onClick, className, children}) =>
     {children}
   </button>
 
-const Table = ({list_c, pattern, onDismiss}) =>
-  <div>
+const Table = ({ list_c, pattern, onDismiss }) =>
+  <div className="table">
     {list_c.filter(isSearched(pattern)).map(item =>
-    <div key={item.objectID}>
-        <span>
-            <a href={item.url} > {item.title}</a>
+      <div key={item.objectID} className="table-row">
+        <span style={{ width: '40%' }}> 
+          <a href={item.url}>{item.title}</a> 
+        </span> 
+        <span style={{ width: '30%' }}> 
+          {item.author} 
+        </span> 
+        <span style={{ width: '10%' }}> 
+          {item.num_comments} 
+        </span> 
+        <span style={{ width: '10%' }}> 
+          {item.points} 
+        </span> 
+        <span style={{ width: '10%' }}> 
+          <Button
+          onClick={() => onDismiss(item.objectID)} 
+          className="button-inline"
+        >
+          Dismiss 
+        </Button>
         </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-        <span>
-            <Button onClick={()=>onDismiss(item.objectID)}>
-                Dismiss
-            </Button>
-        </span>
-    </div>
+      </div>
     )}
-</div>
+  </div>
 
 
 class App extends Component {
@@ -87,30 +96,32 @@ class App extends Component {
   onDismiss(id) {
     const isNotId = item => item.objectID !== id;
     const updatedList = this.state.list_b.filter(isNotId);
-    this.setState({list_b: updatedList});
+    this.setState({ list_b: updatedList });
   }
 
   onSearchChange(event) {
-    this.setState({searchTerm: event.target.value});
+    this.setState({ searchTerm: event.target.value });
   }
 
   render() {
 
     const { list_b, searchTerm } = this.state;
-    
+
     return (
-      <div className="App">
-        <Search 
-          value={searchTerm} 
-          onChange={this.onSearchChange} 
-        >
-          Search
+      <div className="page">
+        <div className="interactions">
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          >
+            Search
         </Search>
-        <Table 
-          list_c={list_b} 
-          pattern={searchTerm} 
-          onDismiss={this.onDismiss} 
-        />
+          <Table
+            list_c={list_b}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
+          />
+        </div>
       </div>
     );
   }
